f1c1c9865d0c0d41e98171c1b9840962666b430f
Dot <danielonyeka09@gmail.com>
2026-09-23 00:59:22 +0100

    Add files via upload

--- bash/04-conditionals/if.sh
+++ bash/04-conditionals/if.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# The basic if
+
 read -p "How old are you? " age
 
 if [ "$age" -ge 18 ]; then
 	echo "You are an adult"
 fi
+
+# [ ... ] is a test. -ge means "greater than or equal to".
+# then starts the block that runs if the test passes.
+# fi (if backwards) closes the block.
